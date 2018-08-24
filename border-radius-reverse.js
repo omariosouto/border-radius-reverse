e05b6b79a11a400aeddf2481871409ba21fe2528
Mario Souto <soutomarios@gmail.com>
2018-08-24 00:51:18 -0300

    update

--- border-radius-reverse.js
+++ border-radius-reverse.js
@@ -1,6 +1,8 @@
+// 2) Criamos uma classe que terá as lógicas
+// para manipular o paint do elemento que tiver usando
 class BorderRadiusReversePainter {
     static get inputProperties(){
-        return ['--border-radius-reverse']
+        return ['--border-radius-reverse', '--background-color']
     }
 
     clearCircle(context,x,y,radius) {
@@ -12,11 +14,13 @@ class BorderRadiusReversePainter {
         context.restore();
     }
 
-    paint(ctx, geom, props){
+    paint(ctx, geom, props){ // 3) ctx tem o elemento
         const radiusValue = props.get('--border-radius-reverse').toString()
+        const bgColor = props.get('--background-color').toString()
         console.log(radiusValue)
+        console.log(ctx)
 
-        ctx.fillStyle = "#ff6a00"; // fixed color
+        ctx.fillStyle = bgColor; // fixed color
         ctx.fillRect(0, 0, geom.width, geom.height);
         
         this.clearCircle(ctx, 0, 0, radiusValue) // Left top
@@ -27,5 +31,6 @@ class BorderRadiusReversePainter {
     }
 }
 
-// Register our class under a specific name
+// 4) Registrando a classe com um nome para chamarmos
+// background-image: paint(border-radius-reverse);
 registerPaint('border-radius-reverse', BorderRadiusReversePainter);
